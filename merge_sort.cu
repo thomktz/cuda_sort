daa10d717dccb8d8f4a218fc01d56369b1e8e881
eliasjeannot <95873267+eliasjeannot@users.noreply.github.com>
2024-04-26 17:56:22 +0200

    kernel merge_sort.cu

--- merge_sort.cu
+++ merge_sort.cu
@@ -6,15 +6,6 @@ the name of the author above.
 ***************************************************************/
 
 #include <stdio.h>
-#include <curand_kernel.h>
-
-__device__ float Y0d[10];
-__device__ float md[10];
-__device__ float alphad[10];;
-__device__ float nu2d[10];
-__device__ float rhod[10];
-__device__ float Strd[4];
-
 
 // Function that catches the error 
 void testCUDA(cudaError_t error, const char* file, int line) {
@@ -27,127 +18,64 @@ void testCUDA(cudaError_t error, const char* file, int line) {
 
 // Has to be defined in the compilation in order to get the correct value of the 
 // macros __FILE__ and __LINE__
-#define testCUDA(error) (testCUDA(error, __FILE__ , __LINE__))>
+#define testCUDA(error) (testCUDA(error, __FILE__ , __LINE__))
 
-void strikeInterval(float *K, float T){
+__global__ void empty_k(void) {}
 
-	float fidx = T*12.0f + 1.0f;
-	int i = 0;
-	float coef = 1.0f;
-	float delta;
+__global__ void merge_k(int *A, int *B, int *M, int length) {
+  /*A, B, M : arrays, length : max(|A|, |B|)*/
 
-	while(i<fidx){
-		coef *= (1.025f);
-		i++;
-	}
-	delta = (coef - 1.0f)*(coef + 1.0f)/(4.0f*coef);
+  i =  threadIdx.x;
+  if (i>length) {
+    int Kx=i-length;
+    int Ky=length;
+    int Px=length;
+    int Py=i-length;
+  } else {
+    int Kx=0;
+    int Ky=i;
+    int Px=i;
+    int Py=0;
+  }
 
-	for(i=0; i<4; i++){
-		K[i] = (i+1.0f)*delta + (1.0f/coef);
-	}
-}
 
-// Set the state for each thread
-__global__ void init_curand_state_k(curandState* state)
-{
-	int idx = blockDim.x * blockIdx.x + threadIdx.x;
-	curand_init(0, idx, 0, &state[idx]);
-}
+  while (true){
+    int offset=abs(Ky-Py)/2;
+    int Qx=Kx+offset;
+    int Qy=Ky-offset;
+    
+    if (Qy >= 0 && Qx <= B.size() && (Qy == A.size() || Qx == 0 || A[Qy] > B[Qx - 1])) {
+      if (Qx == B.size() || Qy == 0 || A[Qy - 1] <= B[Qx]) {
+        if (Qy < A.size() && (Qx == B.size() || A[Qy] <= B[Qx])) {
+          M[i]=A[Qy];
+        } else {
+          M[i]=B[Qx];
+        }
+        break;
+      } else{
+        Kx=Qx+1;
+        Ky=Qy-1;
+      }
+    } else {
+      Px=Qx-1;
+      Py=Qy+1;
+    }
+  }
 
-// Monte Carlo simulation kernel
-__global__ void MC_k(float dt, float T, int Ntraj, curandState* state, float* sum){
+	
 
-	int pidx, same;
-	float t, X, Y;
-	int idx = blockDim.x * blockIdx.x + threadIdx.x; 
-	curandState localState = state[idx];
-	float2 G;
-	float B;
-	float price;
-	float sumR = 0.0f;
-	float sum2R = 0.0f;
-	float StrR, mR, alphaR, betaR, rhoR, nu2R;
-
-	/***********************************************************************
-		
-	How should we fill StrR, mR, alphaR, nu2R, rhoR?
-
-	************************************************************************/
-
-	betaR = sqrtf(2.0f*alphaR*nu2R)*(1.0f - expf(mR));
-	for (int i = 0; i < Ntraj; i++) {
-		/***********************************************************************
-		
-		Put your code here
-
-		************************************************************************/
-	}
-	sum[2*idx] = sumR;
-	sum[2*idx + 1] = sum2R;
-
-	/* Copy state back to global memory */
-	state[idx] = localState;
 }
 
 int main(void) {
 
-	float Y0[10] = {logf(0.4f), logf(0.35f), logf(0.31f), logf(0.27f), logf(0.23f), 
-					logf(0.2f), logf(0.17f), logf(0.14f), logf(0.11f), logf(0.08f)};
-	float m[10] = {logf(0.34f), logf(0.3f), logf(0.27f), logf(0.24f), logf(0.21f), 
-					logf(0.18f), logf(0.15f), logf(0.12f), logf(0.09f), logf(0.06f)};
-	float alpha[10] = {0.2f, 0.5f, 1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f};
-	float nu2[10] = {0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f};
-	float rho[10] = {0.95f, 0.75f, 0.55f, 0.35f, 0.15f, -0.15f, -0.35f, -0.55f, -0.75f, -0.95f};
-	float Tmt[4] = {3.0f/12.0f, 6.0f/12.0f, 1.0f, 2.0f};
-	float Str[4];
+	empty_k <<<1, 1>>> ();
 
+	/*************************************************************
 
+	Once requested, replace this comment by the appropriate code
 
-	cudaMemcpyToSymbol(Y0d, Y0, 10*sizeof(float));
-	cudaMemcpyToSymbol(md, m, 10*sizeof(float));
-	cudaMemcpyToSymbol(alphad, alpha, 10*sizeof(float));
-	cudaMemcpyToSymbol(nu2d, nu2, 10*sizeof(float));
-	cudaMemcpyToSymbol(rhod, rho, 10*sizeof(float));
+	*************************************************************/
 
-	int NTPB = 640;
-	int NB = 625;
-	int Ntraj = 100000;
-	float dt = sqrtf(1.0f/(64.0f*12.0f));
-	float StrR, mR, alphaR, betaR, rhoR, YR, price, error;
-
-	curandState* states;
-	cudaMalloc(&states, NB*NTPB*sizeof(curandState));
-	init_curand_state_k <<<NB, NTPB>>> (states);
-	float *sum;
-	cudaMallocManaged(&sum, 2*NB*NTPB*sizeof(float));
-
-	FILE* fpt;
-
-	char strg[20];
-	for(int i=0; i<4; i++){
-		strikeInterval(Str, Tmt[i]);
-		cudaMemcpyToSymbol(Strd, Str, 4*sizeof(float));
-		MC_k<<<NB,NTPB>>>(dt, Tmt[i], Ntraj, states, sum);
-		cudaDeviceSynchronize();
-		for(int j=0; j<4; j++){
-			StrR = Str[j];
-			sprintf(strg, "Tmt%.2fStr%.2f.csv", Tmt[i], StrR);
-			fpt = fopen(strg, "w+");
-			fprintf(fpt, "alpha, beta, m, rho, Y0, price, 95cI\n");
-			for(int k=0; k< 640*625; k++){
-				/***********************************************************************
-					
-				How should we fill StrR, mR, alphaR, betaR, rhoR, YR, price and error?
-
-				************************************************************************/
-				fprintf(fpt, "%f, %f, %f, %f, %f, %f, %f\n", alphaR, betaR, mR, rhoR, YR, price, error);
-			}
-			fclose(fpt);
-		}
-	}
-
-	cudaFree(states);
-	cudaFree(sum);
 
 	return 0;
 }
