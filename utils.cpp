be3feeaf1a428412a26c2a11d1b1bfe9d9003a85
eliasjeannot <95873267+eliasjeannot@users.noreply.github.com>
2024-05-02 15:04:54 +0200

    add isSorted change int to double utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -16,15 +16,15 @@ void insertionSort(int arr[], int n) {
   }
 }
 
-void printArray(int *array, int size) {
+void printArray(double *array, int size) {
   // Utils function to print an array
 
   printf("[");
   // (Assumes length is > 0)
   for (int i = 0; i < size-1; i++) {
-    printf("%d, ", array[i]);
+    printf("%f, ", array[i]);
   }
-  printf("%d]", array[size-1]);
+  printf("%f]", array[size-1]);
   printf("\n");
 }
 
@@ -35,4 +35,25 @@ void generateRandomSortedArray(int *array, int max_value, int size) {
     array[i] = rand() % (max_value + 1);
   }
   insertionSort(array, size);
+}
+
+void fastGenerateRandomSortedArray(int *array, int max_value, int size) {
+  // Utils function to generate a random sorted array
+  array[0] = rand() % (max_value + 1);
+
+  for (int i = 1; i < size; i++) {
+    array[i] = array[i-1] + rand() % (max_value + 1);
+  }
+}
+
+bool isSorted(int *a, int size){
+  // Utils function to verify if an array is sorted
+  bool sorted = true;
+  for (int i =0; i<size-1; i++){
+    if (a[i] > a[i+1]){
+      sorted = false;
+      break;
+    }
+  }
+  return (sorted);
 }
